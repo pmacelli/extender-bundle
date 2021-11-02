8d299e3a4c34fab7c7e3f8470dffd30478a11e26
pmacelli <91888876+pmacelli@users.noreply.github.com>
2021-11-02 16:27:37 +0100

    Update WorklogsVisualizer.php

--- src/Comodojo/Extender/Commands/Utils/WorklogsVisualizer.php
+++ src/Comodojo/Extender/Commands/Utils/WorklogsVisualizer.php
@@ -86,7 +86,12 @@ class WorklogsVisualizer {
                 ['id', 'pid', 'jid', 'uid', 'puid', 'task', 'status', 'start', 'end']
             ]
         );
+        
         foreach ($wklgs as $wklg) {
+            
+            //PHP Fatal error:  Uncaught TypeError: Argument 1 passed to Symfony\Component\Console\Helper\FormatterHelper::truncate() must be of the type string, null given            
+            if(is_null($wklg['parent_uid'])) $wklg['parent_uid'] = "";
+            
             $table->addRow([
                 $wklg['id'],
                 $wklg['pid'],
